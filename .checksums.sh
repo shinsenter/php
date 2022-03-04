daa05a982e3f5fd66fcf5ebe6d2f1b6873b5cdf8
Shin <2082119+shinsenter@users.noreply.github.com>
2022-03-04 16:25:35 +0900

    feat: add build files (#7)

---
+++ .checksums.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+BASE_DIR="$(git rev-parse --show-toplevel)"
+HASH_DIR="$BASE_DIR/.checksums"
+WORKFLOW=.github/workflows/production.yml
+
+curlhash () {
+    local output=${HASH_DIR}/${1:-checksums.txt}
+    local url=$2
+    echo "$(curl -sL "$url" | shasum -a 256 | cut -c1-40) $url" >$output
+}
+
+treehash() {
+    local output=${HASH_DIR}/${1:-checksums.txt};shift
+    local location=$@
+    >$output
+    for path in $location; do
+        if [ -f ${HASH_DIR}/$path ]; then
+            cat ${HASH_DIR}/$path >>$output
+        else
+            echo "$(git rev-parse HEAD:$path | cut -c1-40) $path" >>$output
+        fi
+    done
+}
+
+mkdir -p $HASH_DIR
+
+curlhash ondrej-php.txt     http://ppa.launchpad.net/ondrej/php/ubuntu
+curlhash ondrej-apache.txt  http://ppa.launchpadcontent.net/ondrej/apache2/ubuntu
+curlhash ondrej-nginx.txt   http://ppa.launchpadcontent.net/ondrej/nginx-mainline/ubuntu
+
+treehash base.txt           $WORKFLOW src/base/
+treehash cli.txt            base.txt ondrej-php.txt src/php/cli/
+treehash fpm.txt            cli.txt src/php/fpm/
+treehash servers.txt        fpm.txt ondrej-apache.txt ondrej-nginx.txt src/servers/
+treehash webapps.txt        servers.txt src/webapps/
