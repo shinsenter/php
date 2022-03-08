3da0e6f03fe61e606b1f7c93b1eaf32cbbda2d56
Shin <2082119+shinsenter@users.noreply.github.com>
2022-03-08 13:23:09 +0900

    fix: use s6 overlay v3.1.0.1 (#10)

--- tests/test.sh
+++ tests/test.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
-export PHP_VERSION=${1:-8.1}
-export BUILD_PLATFORM=${2:-linux/arm/v7}
+BASE_DIR="$(git rev-parse --show-toplevel)"
+TEST_DIR="$BASE_DIR/tests"
+
 export DOCKER_BUILDKIT=1
+export S6_VERSION="$(cat $BASE_DIR/S6_VERSION.txt)"
+export PHP_VERSION="${1:-8.1}"
+export BUILD_PLATFORM="${2:-linux/arm/v6}"
 
-BASE_DIR="$(git rev-parse --show-toplevel)/tests"
+clear
+$BASE_DIR/.git-config/hooks/pre-commit
 
-clear \
-&& bash $BASE_DIR/../.git-config/hooks/pre-commit \
-&& echo "Starting building php:${PHP_VERSION} images for platform ${BUILD_PLATFORM}..." \
-&& docker-compose -f $BASE_DIR/docker-compose.base.yml build \
-&& docker-compose -f $BASE_DIR/docker-compose.cli.yml build \
-&& docker-compose -f $BASE_DIR/docker-compose.fpm.yml build \
-&& docker-compose -f $BASE_DIR/docker-compose.server.yml build \
-&& docker-compose -f $BASE_DIR/docker-compose.webapps.yml build \
+echo
+echo "🤖 Starting building S6 ${S6_VERSION} and \
+php:${PHP_VERSION} images for platform ${BUILD_PLATFORM}..."
+
+echo && date \
+&& docker-compose -f $TEST_DIR/docker-compose.base.yml build \
+&& docker-compose -f $TEST_DIR/docker-compose.cli.yml build \
+&& docker-compose -f $TEST_DIR/docker-compose.fpm.yml build \
+&& docker-compose -f $TEST_DIR/docker-compose.server.yml build \
+&& docker-compose -f $TEST_DIR/docker-compose.webapps.yml build \
 && echo "Finished."
