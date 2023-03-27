6e84136286cbad7a58c5f91b0ee783ec5c5434be
shinsenter <2082119+shinsenter@users.noreply.github.com>
2023-03-27 17:30:09 +0900

    fix: improve config

--- src/webapps/wordpress/root/etc/docker-wordpress.php
+++ src/webapps/wordpress/root/etc/docker-wordpress.php
@@ -4,10 +4,30 @@ function docker_config($config_path)
 {
     try {
         if (file_exists($config_path)) {
+            $content = str_replace(' * @package WordPress
+ */', ' * @package WordPress
+ */
+
+if (!function_exists(\'getenv_docker\')) {
+	// a helper function to lookup "env_FILE", "env", then fallback
+	function getenv_docker($env, $default) {
+		if ($fileEnv = getenv($env . \'_FILE\')) {
+			return rtrim(file_get_contents($fileEnv), "\r\n");
+		}
+
+		if (($val = getenv($env)) !== false) {
+			return $val;
+		}
+
+		return $default;
+	}
+}
+', file_get_contents($config_path));
+
             $content = preg_replace(
-                '/^define\( *["\']([a-z0-9_]+)["\'],( *)((?!.*getenv *\( *\'WORDPRESS_).+?) *\) *;/mi',
-                'define( \'$1\',$2getenv(\'WORDPRESS_$1\') ?: $3 );',
-                file_get_contents($config_path)
+                '/^define\( *["\']([a-z0-9_]+)["\'],( *)((?!.*getenv_docker *\( *\'WORDPRESS_).+?) *\) *;/mi',
+                'define( \'$1\',$2getenv_docker(\'WORDPRESS_$1\') ?: $3 );',
+                $content
             );
 
             if (!empty($content)) {
