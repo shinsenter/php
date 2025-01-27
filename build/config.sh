0b16781e7af7e01865fcfc8afe6897b0a10aabc9
Shin <2082119+shinsenter@users.noreply.github.com>
2025-01-27 18:59:54 +0900

    chore: modify build platform for the nginx unit variant (#185)

--- build/config.sh
+++ build/config.sh
@@ -139,6 +139,9 @@ with-unit)
     BUILD_CACHE_KEY="(unit@$(echo "$unit_version" | head -c17))"
     PHP_VARIANT="zts$SUFFIX"
     ALLOW_RC=1
+    if [ "$OS_BASE" == "alpine" ]; then
+        BUILD_PLATFORM="linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64/v8"
+    fi
     ;;
 with-f8p)
     verlt "$PHP_VERSION" "8.2" && SKIP_BUILD=1
